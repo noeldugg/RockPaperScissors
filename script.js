6b9b8f7f58a9f05c35dd83a841e96280a4736375
Noel Duggan <noelduggan21@gmail.com>
2024-06-10 11:50:39 +0000

    Declared variables to the script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+
+
+const playerScore  = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const choices = ["rock","paper","scissors"];
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const 
+const result = document.getElementById("result");
+
+
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+
+rock.addEventListener("click", () => game("rock"));
+paper.addEventListener("click", () => game("paper"));
+scissors.addEventListener("click",() => game("scissors"));
+
+ 
+function game(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+ 
+}
